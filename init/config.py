692be9e010c9866a03ec1184cffde3fe77a63f5d
Ji Bo <bo.ji@intel.com>
2013-06-19 15:35:49 +0800

    customize my basic information
    Signed-off-by: Ji Bo <bo.ji@intel.com>

--- init/config.py
+++ init/config.py
@@ -8,6 +8,6 @@ ENV_URL = os.path.join(HOME,PROJECT_FOLDER,'config')
 #nfs mount dirname
 EMBED_WORK = 'Embbed_work'
 #use to generate ssh public key
-EMAIL = 'jibo.tiger@inel.com'
+EMAIL = 'jibo.tiger@gmail.com'
 #NET_AGENCY is network agency, According to individual needs to add or don't add
 NET_AGENCY = ' '
