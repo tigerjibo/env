003bfdacbbce242731dfa19772934af261902cbd
Ji Bo <jibo.tiger@gmail.com>
2014-03-11 17:54:57 +0800

    更新交叉编译连环境
    Signed-off-by: Ji Bo <jibo.tiger@gmail.com>

---
+++ config/bin_script/setup-minicom.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+cwd=`dirname $0`
+. $cwd/common.sh
+
+
+minicomcfg=${HOME}/.minirc.dfl
+
+echo
+echo "--------------------------------------------------------------------------------"
+echo "This step will set up minicom (serial communication application) for"
+echo "SDK development"
+echo
+echo
+echo "For boards that contain a USB-to-Serial converter on the board (BeagleBone), "
+echo "the port used for minicom will be automatically detected. By default Ubuntu "
+echo "will not recognize this device. Setup will add a udev rule to "
+echo "/etc/udev/ so that from now on it will be recognized as soon as the board is "
+echo "plugged in."
+echo
+echo "For other boards, the serial will defualt to /dev/ttyUSB0. Please update based "
+echo "on your setup."
+
+echo "--------------------------------------------------------------------------------"
+echo
+
+portdefault=/dev/ttyUSB0
+
+echo ""
+echo "NOTE: For boards with a built-in USB to Serial adapter please press"
+echo "      ENTER at the prompt below.  The correct port will be determined"
+echo "      automatically at a later step.  For all other boards select"
+echo "      the serial port that the board is connected to"
+echo "Which serial port do you want to use with minicom?"
+read -p "[ $portdefault ] " port
+
+if [ ! -n "$port" ]; then
+    port=$portdefault
+fi
+
+if [ -f $minicomcfg ]; then
+    cp $minicomcfg $minicomcfg.old
+    echo
+    echo "Copied existing $minicomcfg to $minicomcfg.old"
+fi
+
+echo "pu port             $port
+pu baudrate         115200
+pu bits             8
+pu parity           N
+pu stopbits         1
+pu minit
+pu mreset
+pu mdialpre
+pu mdialsuf
+pu mdialpre2
+pu mdialsuf2
+pu mdialpre3
+pu mdialsuf3
+pu mconnect
+pu mnocon1          NO CARRIER
+pu mnocon2          BUSY
+pu mnocon3          NO DIALTONE
+pu mnocon4          VOICE
+pu rtscts           No" | tee $minicomcfg > /dev/null
+check_status
+
+echo
+echo "Configuration saved to $minicomcfg. You can change it further from inside"
+echo "minicom, see the Software Development Guide for more information."
+echo "--------------------------------------------------------------------------------"
--- init/config.py
+++ init/config.py
@@ -2,8 +2,8 @@ import os
 #PROJECT_FOLDER is the project folder name
 PROJECT_FOLDER = 'env'
 HOME = os.environ['HOME']
-HOST_NAME =  (HOME.split('/'))[2]
-CONFIG = 'CV_Linux'
+HOST_NAME =  (HOME.split('/'))[1]
+CONFIG = 'ED_Linux'
 BASE = os.path.join(HOME, CONFIG)
 ENV_URL = os.path.join(HOME,PROJECT_FOLDER,'config')
 #nfs mount dirname
